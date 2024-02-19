504f6a2bfd44fa91b0dacc83fb080e1286153992
Volodymyr Babych <domonock13@icloud.com>
2024-02-19 13:13:05 +0200

    Initial Commit

---
+++ TradingPanelT/TradingPanelTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingPanelTApp.swift
+//  TradingPanelT
+//
+//  Created by Volodymyr Babych on 19.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TradingPanelTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
